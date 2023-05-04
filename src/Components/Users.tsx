09d416a383572939888d22de1fbcd38abaa1cc0a
Brian <oshinbolubrian@gmai;.com>
2023-05-04 19:01:54 +0100

    data added

--- src/Components/Users.tsx
+++ src/Components/Users.tsx
@@ -10,14 +10,16 @@ const Users = () => {
 
     const [users, setUsers] = useState([]);
 
-    const fetchUserData = () => {
-    fetch("https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users")
-      .then(response => response.json())
-      .then(data => setUsers(data))
-    };
+    // const fetchUserData = () => {
+    // fetch("https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users")
+    //   .then(response => response.json())
+    //   .then(data => setUsers(data))
+    // };
 
     useEffect(() => {
-        fetchUserData();
+        fetch("https://6270020422c706a0ae70b72c.mockapi.io/lendsqr/api/v1/users")
+        .then(response => response.json())
+        .then(data => setUsers(data))
     }, []);
 
 
@@ -34,7 +36,7 @@ const Users = () => {
                 <div className="card">
                     <NumUser />
                     <p>USERS</p>
-                    <p className="num">2453</p>
+                    <p className="num">{users.length}</p>
                 </div>
                 <div className="card">
                     <ActiveUsers />
@@ -53,16 +55,32 @@ const Users = () => {
                 </div>
             </div>
             <div className="user-list">
-                <div className="table-head">
-                    <h4>ORGANIZATION <Filter /></h4>
-                    <h4>USERNAME <Filter /></h4>
-                    <h4>EMAIL <Filter /></h4>
-                    <h4>PHONE NUMBER <Filter /></h4>
-                    <h4>DATE JOINED <Filter /></h4>
-                    <h4>STATUS <Filter /></h4>
-                </div>
-                <div>
-                    <p>{!users ? 'sup' : 'something'}</p>
+                <div className="table">
+                    <table>
+                        <thead>
+                        <tr>
+                            <th>ORGANIZATION <Filter /></th>
+                            <th>USERNAME <Filter /></th>
+                            <th>EMAIL <Filter /></th>
+                            <th>PHONE NUMBER <Filter /></th>
+                            <th>DATE JOINED <Filter /></th>
+                            <th>STATUS <Filter /></th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {users.length > 0 && 
+                                users.map(user => (
+                                <tr key={user.id}>
+                                    <td>{user.orgName}</td>
+                                    <td>{user.userName}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phoneNumber}</td>
+                                    <td>{user.createdAt}</td>
+                                    <td>{user.email}</td>
+                                </tr>))
+                            }
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
